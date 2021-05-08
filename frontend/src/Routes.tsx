2dd62a9ab8f661f1c9694f1bd62eb1595d4eba1c
Rodrigo P <rodrigoprestes3@yahoo.com.br>
2021-05-08 20:05:33 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,16 +4,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const Routes = () => {
     return (
+
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact>
+
                     <Home />
                 </Route>
-                <Route path="/dashboard" >
+                <Route path="/dashboard">
+
                     <Dashboard />
                 </Route>
             </Switch>
         </BrowserRouter>
+
     );
 }
+
 export default Routes;
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,44 +1,36 @@
 import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
-import DonutChart from "components/DonutChart"
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 const Dashboard = () => {
     return (
         <>
-        <NavBar />
-        <div className="container">
-           <h1 className="text-primary py-3">Dashboard de vendas</h1>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-           <div className="row px-3">
-              <div className="col-sm-6">
-                 <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
-                 <BarChart />
-                 </div>
+                <div className="row px-3">
 
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                        <BarChart />
+                    </div>
 
-              </div>
-              <div className="row px-3">
-                 <div className="col-sm-6">
-                    <h5 className="text center text-secondary">Todas vendas</h5>
-
-                    <DonutChart />
-  
-                 </div>
-                 </div>
-                 <div className="py-3">
-                    <h2 className="text-primary">Todas as vendas</h2>
-
-                 </div>
-                 <DataTable />
-              </div>
-              <Footer />
-     </>
-
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Vendas Totais</h5>
+                        <DonutChart />
+                    </div>
+                    <div className="py-3">
+                        <h2 className="text-primary">Todas Vendas</h2>
+                    </div>
+                </div>
+                <DataTable />
+            </div>
+            <Footer />
+        </>
     );
-
-
 }
 
 export default Dashboard;
